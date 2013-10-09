7b0801237bfde358b349a6401ee938fab8951e1e
Apprentice <apprentice@dbc15.local>
2013-10-09 12:14:53 -0700

    Add name column to tags, validated uniqueness of name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131009180133) do
+ActiveRecord::Schema.define(version: 20131009191120) do
 
   create_table "resources", force: true do |t|
     t.datetime "created_at"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20131009180133) do
   create_table "tags", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "topic_tags", force: true do |t|
--- models/tag.rb
+++ models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
   has_many :topic_tags
   has_many :topics, through: :topic_tags
-
+  validates_uniqueness_of :name
 end
