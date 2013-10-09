cac8c2519364c91fd97caa20be7071842c098909
Apprentice <apprentice@dbc18.local>
2013-10-09 11:19:01 -0700

    Validates numericality for topic id, test passes

--- resource_library_app.rb
+++ resource_library_app.rb
@@ -22,6 +22,7 @@ end
 class Resource < ActiveRecord::Base
   belongs_to :topic
   validates :url, presence: true
+  validates :topic_id, numericality: true
   validates :url, format: { with: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/, message: "Please enter a valid url" }
 
 end
