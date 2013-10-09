cf4014fbe80cd7b31c5ed85dfac4b339aef63214
Apprentice <apprentice@dbc29.local>
2013-10-09 10:26:55 -0700

    Passed seventh test on Level 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131009170125) do
+ActiveRecord::Schema.define(version: 20131009172343) do
 
   create_table "resources", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "topic_id"
+    t.string   "url"
   end
 
   create_table "tags", force: true do |t|
