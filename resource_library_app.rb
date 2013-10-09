a8bfe1799973e19f9e27df7d2584949acdb6f21b
Apprentice <apprentice@dbc17.local>
2013-10-09 12:41:17 -0700

    tag has many topics through topic_tags table

--- resource_library_app.rb
+++ resource_library_app.rb
@@ -5,6 +5,8 @@ set :database, 'sqlite3:///db/resource_library.sqlite3'
 
 class Topic < ActiveRecord::Base
 	has_many :resources
+	# has_many :topic_tags
+	has_many :tags, through: :topic_tags
 
 	validates :name, :opinion, presence: true
 	validates :opinion, length: { minimum: 10 }
@@ -30,9 +32,12 @@ class Resource < ActiveRecord::Base
 end
 
 class TopicTag < ActiveRecord::Base
-
+	belongs_to :topic
+	belongs_to :tag
 end
 
 class Tag < ActiveRecord::Base
+	has_many :topic_tags
+	has_many :topics, through: :topic_tags
 
 end
