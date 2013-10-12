7594e5c38c7dbd70b72e27d75d0b3973b3cd8f0b
Apprentice <apprentice@dbc29.local>
2013-10-12 13:35:10 -0700

    validates numericality of topic_id

--- resource_library_app.rb
+++ resource_library_app.rb
@@ -22,8 +22,10 @@ end
 class Resource < ActiveRecord::Base
 
   belongs_to :topic
+
   validates :url, presence: true
   validates_format_of :url, :with => URI::regexp(%w(http https))
+  validates :topic_id, numericality: true
 
 end
 
