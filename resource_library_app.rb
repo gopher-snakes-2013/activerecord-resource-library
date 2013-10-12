e0dd1d2dcc939ec0c4e9324882f0351ffe1e14b2
Apprentice <apprentice@dbc29.local>
2013-10-12 13:30:10 -0700

    validates urls

--- resource_library_app.rb
+++ resource_library_app.rb
@@ -23,6 +23,8 @@ class Resource < ActiveRecord::Base
 
   belongs_to :topic
 
+  validates_format_of :url, :with => URI::regexp(%w(http https))
+
 end
 
 class TopicTag < ActiveRecord::Base
--- spec/level_1_spec.rb
+++ spec/level_1_spec.rb
@@ -21,7 +21,7 @@ describe Resource do
 
   describe "#url" do
     it { should allow_value("http://google.com/").for(:url) }
-    xit { should_not allow_value("an invalid url").for(:url) }
+    it { should_not allow_value("an invalid url").for(:url) }
     xit { should validate_presence_of(:url) }
   end
 
