d359537e98f7162cbec804d186e6b1ebbfd25798
Apprentice <apprentice@dbc29.local>
2013-10-12 13:21:08 -0700

    validates opinion length 15+

--- resource_library_app.rb
+++ resource_library_app.rb
@@ -8,7 +8,7 @@ class Topic < ActiveRecord::Base
   has_many :resources
 
   validates :name, presence: true
-  validates :opinion, presence: true
+  validates :opinion, presence: true, length: { minimum: 15 }
 
   def tag_with!(tag)
     # IMPLEMENT ME
